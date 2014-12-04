c183aab317619f7b6fe5a4dc3308dc584470d284
Luc Boutier <luc.boutier@me.com>
2014-12-04 17:16:31 +0100

    Fixed gist loading.

--- js/navigation.js
+++ js/navigation.js
@@ -1,7 +1,6 @@
 $(function(){
   // Bind the event.
   $(window).hashchange( function(){
-    // console.log('{{root}}');
     var hash = location.hash.replace( /^#/, '' );
     if(hash && hash!== null && hash.match(/html$/)) {
       $("#content-parent").load(hash);
